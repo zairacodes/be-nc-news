4cecf5c505b74140e8e4a6cbc9dd3dab9d835772
zairacodes <zaira.nuovo@gmail.com>
2024-05-30 18:40:58 +0100

    update fetchArticle query in model to include comment_count in response

--- models/articles.models.js
+++ models/articles.models.js
@@ -21,7 +21,10 @@ exports.fetchArticles = (topic) => {
 
 exports.fetchArticle = (article_id) => {
   return db
-    .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
+    .query(
+      "SELECT articles.*, COUNT(comments.comment_id)::INT AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id;",
+      [article_id]
+    )
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Not Found" });
