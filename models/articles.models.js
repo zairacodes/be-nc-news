a4b55bfa7457f7d3f15d9e1976e1debb0272c693
zairacodes <zaira.nuovo@gmail.com>
2024-05-30 15:54:19 +0100

    create an endpoint for GET /api/articles (topic query)

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,13 +1,22 @@
 const db = require("../db/connection");
 
-exports.fetchArticles = () => {
-  return db
-    .query(
-      "SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id)::INT AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC;"
-    )
-    .then(({ rows }) => {
-      return rows;
-    });
+exports.fetchArticles = (topic) => {
+  let sqlQuery =
+    "SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id)::INT AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id";
+
+  const queryValues = [];
+
+  if (topic) {
+    sqlQuery += " WHERE articles.topic = $1";
+    queryValues.push(topic);
+  }
+
+  sqlQuery +=
+    " GROUP BY articles.article_id ORDER BY articles.created_at DESC;";
+
+  return db.query(sqlQuery, queryValues).then(({ rows }) => {
+    return rows;
+  });
 };
 
 exports.fetchArticle = (article_id) => {
