64d1580fac655d9a99f3ee1b3819c0445ba8c9dc
zairacodes <zaira.nuovo@gmail.com>
2024-05-30 18:38:33 +0100

    update test for GET /api/articles/:article_id to include comment_count in response

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -120,6 +120,7 @@ describe("GET /api/articles/:article_id", () => {
           votes: 0,
           article_img_url:
             "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          comment_count: 0,
         });
       });
   });
